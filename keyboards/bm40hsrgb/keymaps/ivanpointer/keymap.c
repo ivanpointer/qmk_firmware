92166f7fc068a5b2f1209bd194936912356d6caa
ivanpointer <ivan@pointerplace.us>
2021-01-24 14:14:30 -0600

    Cleanup formatting of ivanpointer keymaps.

--- keyboards/bm40hsrgb/keymaps/ivanpointer/keymap.c
+++ keyboards/bm40hsrgb/keymaps/ivanpointer/keymap.c
@@ -6,6 +6,10 @@
  * edit it directly.
  */
 
+#define _____ KC_NO
+//      __VVV__
+#define __VVV__ KC_TRNS
+
 enum layers {
 	_DVORAK,
 	_SHIFT2,
@@ -17,13 +21,47 @@ enum layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_DVORAK] = LAYOUT_planck_mit(KC_ESC, KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSPC, MO(_SHIFT2), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, MO(_SHIFT2), KC_LSFT, KC_QUOT, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, KC_LCTL, KC_LGUI, KC_LALT, TT(_NUMPAD), TT(_ARROWS), KC_SPC, TT(_FNKEYS), TT(_MEDIA), KC_RALT, KC_RGUI, KC_RCTL),
-	[_SHIFT2] = LAYOUT_planck_mit(TO(_DVORAK), KC_NO, KC_LT, KC_GT, KC_DLR, KC_AMPR, KC_MINS, KC_SLSH, KC_BSLS, KC_AT, KC_HASH, KC_DEL, KC_TRNS, KC_LBRC, KC_LCBR, KC_RCBR, KC_LPRN, KC_EQL, KC_ASTR, KC_RPRN, KC_PLUS, KC_RBRC, KC_EXLM, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_TILD, KC_PERC, KC_UNDS, KC_QUES, KC_PIPE, KC_CIRC, KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TAB, KC_ENT, KC_TAB, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS),
-	[_ARROWS] = LAYOUT_planck_mit(TO(_DVORAK), KC_NO, KC_COPY, KC_PSTE, KC_CUT, KC_NO, KC_NO, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_BSPC, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_DEL, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_ENT, KC_TAB, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS),
-	[_FNKEYS] = LAYOUT_planck_mit(TO(_DVORAK), KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_NO, KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_NO, KC_F17, KC_F18, KC_F19, KC_F20, KC_NO, KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_F21, KC_F22, KC_F23, KC_F24, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS),
-	[_NUMPAD] = LAYOUT_planck_mit(TO(_DVORAK), KC_NO, KC_COPY, KC_PSTE, KC_CUT, KC_NO, KC_PAST, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_BSPC, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_PSLS, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NLCK, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_PENT, KC_P0, KC_P0, KC_TRNS, KC_TRNS, KC_TRNS),
-	[_MEDIA] = LAYOUT_planck_mit(TO(_DVORAK), TO(_SETTINGS), KC_NO, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_SLCK, KC_PAUS, KC_PSCR, KC_CAPS, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_APP, KC_TRNS, KC_NO, KC_NO, KC_MPLY, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[_SETTINGS] = LAYOUT_planck_mit(TO(_DVORAK), KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, RGB_MOD, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, RGB_RMOD, RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_BRTG, BL_STEP, BL_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+	[_DVORAK] = LAYOUT_planck_mit(
+		KC_ESC, 		KC_SCLN, 		KC_COMM, 	KC_DOT, 		KC_P, 			KC_Y, 		KC_F, 		KC_G, 			KC_C, 			KC_R, 		KC_L, 		KC_BSPC,
+		MO(_SHIFT2),	KC_A,	 		KC_O, 		KC_E, 			KC_U, 			KC_I, 		KC_D, 		KC_H, 			KC_T, 			KC_N, 		KC_S, 		MO(_SHIFT2),
+		KC_LSFT, 		KC_QUOT, 		KC_Q, 		KC_J, 			KC_K, 			KC_X, 		KC_B, 		KC_M, 			KC_W, 			KC_V, 		KC_Z, 		KC_RSFT,
+		KC_LCTL,		KC_LGUI, 		KC_LALT, 	TT(_NUMPAD), 	TT(_ARROWS), 		KC_SPC,				TT(_FNKEYS), 	TT(_MEDIA), 	KC_RALT, 	KC_RGUI, 	KC_RCTL),
+
+	[_SHIFT2] = LAYOUT_planck_mit(
+		TO(_DVORAK), 	_____, 			KC_LT, 		KC_GT, 			KC_DLR, 		KC_AMPR, 	KC_MINS, 	KC_SLSH, 		KC_BSLS, 		KC_AT, 		KC_HASH, 	KC_DEL,
+		__VVV__, 		KC_LBRC, 		KC_LCBR, 	KC_RCBR, 		KC_LPRN, 		KC_EQL, 	KC_ASTR, 	KC_RPRN, 		KC_PLUS, 		KC_RBRC, 	KC_EXLM, 	__VVV__,
+		__VVV__,		_____, 			_____, 		_____, 			KC_TILD, 		KC_PERC, 	KC_UNDS, 	KC_QUES, 		KC_PIPE, 		KC_CIRC, 	KC_GRV, 	__VVV__,
+		__VVV__, 		__VVV__, 		__VVV__, 	_____, 			KC_TAB, 			KC_ENT, 			KC_TAB, 		__VVV__, 		_____,		__VVV__, 	__VVV__),
+
+	[_ARROWS] = LAYOUT_planck_mit(
+		TO(_DVORAK), 	_____, 			KC_COPY, 	KC_PSTE, 		KC_CUT, 		_____, 		_____, 		KC_HOME, 		KC_UP, 			KC_END, 	KC_PGUP, 	KC_BSPC,
+		_____, 			KC_LSFT, 		KC_LCTL, 	KC_LGUI, 		KC_LALT, 		_____, 		_____, 		KC_LEFT, 		KC_DOWN, 		KC_RGHT, 	KC_PGDN, 	KC_DEL,
+		__VVV__, 		_____, 			_____, 		_____, 			_____, 			_____, 		_____, 		_____, 			_____, 			_____, 		_____, 		__VVV__,
+		__VVV__, 		__VVV__, 		__VVV__, 	_____, 			__VVV__, 			KC_ENT, 			KC_TAB, 		_____, 			__VVV__, 	__VVV__, 	__VVV__),
+
+	[_FNKEYS] = LAYOUT_planck_mit(
+		TO(_DVORAK), 	KC_F1, 			KC_F2, 		KC_F3, 			KC_F4, 			_____, 		_____, 		KC_F13, 		KC_F14, 		KC_F15, 	KC_F16, 	_____,
+		_____, 			KC_F5, 			KC_F6, 		KC_F7, 			KC_F8, 			_____, 		_____, 		KC_F17, 		KC_F18, 		KC_F19, 	KC_F20, 	_____,
+		__VVV__, 		KC_F9, 			KC_F10, 	KC_F11, 		KC_F12, 		_____, 		_____, 		KC_F21, 		KC_F22, 		KC_F23, 	KC_F24, 	__VVV__,
+		__VVV__, 		__VVV__, 		__VVV__, 	_____, 			_____, 				_____, 				__VVV__, 		_____, 			__VVV__, 	__VVV__, 	__VVV__),
+
+	[_NUMPAD] = LAYOUT_planck_mit(
+		TO(_DVORAK), 	_____, 			KC_COPY, 	KC_PSTE, 		KC_CUT, 		_____, 		KC_PAST, 	KC_P7, 			KC_P8, 			KC_P9, 		KC_PMNS, 	KC_BSPC,
+		_____, 			KC_LSFT, 		KC_LCTL, 	KC_LGUI, 		KC_LALT, 		_____, 		KC_PSLS, 	KC_P4, 			KC_P5, 			KC_P6, 		KC_PPLS, 	_____,
+		__VVV__, 		_____, 			_____, 		_____, 			_____, 			_____, 		KC_NLCK, 	KC_P1, 			KC_P2, 			KC_P3, 		KC_PDOT, 	__VVV__,
+		__VVV__, 		__VVV__, 		__VVV__, 	__VVV__, 		_____, 				KC_PENT,			KC_P0, 			KC_P0, 			__VVV__, 	__VVV__, 	__VVV__),
+
+	[_MEDIA] = LAYOUT_planck_mit(
+		TO(_DVORAK), 	TO(_SETTINGS),	_____, 		KC_MPRV, 		KC_MNXT, 		_____, 		_____, 		_____, 			_____,			KC_SLCK, 	KC_PAUS, 	KC_PSCR,
+		KC_CAPS, 		_____, 			_____, 		KC_VOLD, 		KC_VOLU, 		_____, 		_____, 		_____, 			_____, 			_____, 		_____, 		KC_APP,
+		__VVV__, 		_____, 			_____, 		KC_MPLY, 		KC_MUTE, 		_____, 		_____, 		_____, 			_____, 			_____, 		_____, 		__VVV__,
+		__VVV__, 		__VVV__, 		__VVV__,	_____, 			_____, 				_____, 				_____, 			__VVV__, 		__VVV__, 	__VVV__, 	__VVV__),
+
+	[_SETTINGS] = LAYOUT_planck_mit(
+		TO(_DVORAK), 	__VVV__, 		_____, 		_____, 			_____, 			_____, 		RGB_VAI, 	RGB_HUI, 		RGB_SAI, 		RGB_SPI, 	RGB_MOD, 	RESET,
+		_____, 			_____, 			_____, 		_____, 			_____, 			_____, 		RGB_VAD, 	RGB_HUD, 		RGB_SAD, 		RGB_SPD, 	RGB_RMOD, 	RGB_TOG,
+		_____, 			_____, 			_____, 		_____, 			_____, 			_____, 		_____, 		_____, 			_____, 			BL_BRTG, 	BL_STEP, 	BL_TOGG,
+		_____, 			_____, 			_____, 		_____, 			_____, 				_____, 				_____, 			_____, 			_____, 		_____, 		_____)
 };
 
 /*            HSV COLORS
@@ -62,7 +100,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define PAL_CNTRL_3 PAL_1
 
 #define PAL_LAYERS PAL_CNTRL_2
-
 
 // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
 const rgblight_segment_t PROGMEM my_dvorak_layer[] = RGBLIGHT_LAYER_SEGMENTS(
